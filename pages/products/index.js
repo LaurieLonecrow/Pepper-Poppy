34a3ef88b58069317858e567b80b46b86c03beb1
Laurie Lonecrow <laurieteresa@gmail.com>
2022-01-03 14:01:50 -0500

    fix: products heading temp spacing

--- pages/products/index.js
+++ pages/products/index.js
@@ -6,7 +6,9 @@ import Heading from '../../components/Heading'
 const Products = () => {
   return (
     <>
+    <div className='p-8'>
     <Logo/>
+    </div>
     <Heading content={`Products`}/>
     </>  
   );
